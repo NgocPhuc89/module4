c589932d68cec6c9a91ef607479d40b03f9c30d3
NgocPhuc89 <ngocphuc966@gmail.com>
2023-08-09 16:22:09 +0700

    up

--- quiz/src/main/resources/js/quiz.js
+++ quiz/src/main/resources/js/quiz.js
@@ -13,13 +13,14 @@ function renderQuiz(){
         let str = '';
         data.forEach(quiz => {
             questions = quiz.questions;
-            let ques = '';
-            quiz.questions.forEach((question,index) => {
+            question = questions[0]
+            let ques = `                       `;
+            // quiz.questions.forEach((question,index) => {
                 let ans = '';
                 question.answers.forEach(answer => {
                     if(question.type === "radio"){
                         ans += ` <div class="form-check">
-              <input class="form-check-input" value="${answer.title}" type="radio" name="ques-${question.id}" id="answer-${answer.id}">
+              <input class="form-check-input" value="${answer.content}" type="radio" name="ques-${question.id}" id="answer-${answer.id}">
               <label class="form-check-label" for="flexRadioDefault1">
                   ${answer.title}.${answer.content}
               </label>
@@ -27,22 +28,27 @@ function renderQuiz(){
                     }
                     else {
                         ans += ` <div class="form-check">
-              <input class="form-check-input" value="${answer.title}" type="checkbox" name="ques-${question.id}" id="answer-${answer.id}">
+              <input class="form-check-input" value="${answer.content}" type="checkbox" name="ques-${question.id}" id="answer-${answer.id}">
               <label class="form-check-label" for="flexRadioDefault1">
                   ${answer.title}.${answer.content}
               </label>
           </div>`
                     }
                 })
-                ques += ` <h4 class="alert alert-primary disabled">${index+1}.${question.content}</h4>
+
+                ques += ` <h4 class="alert alert-primary disabled">${1}.${question.content}</h4>
             ${ans}
-          `
-            })
+                    `
+            // })
             str += ` <h1 align="center">${quiz.content}</h1>
-  <div id="">
-      <div id="question">
-         ${ques}
+                     <div id="a">
+                  <div id="questions">
+                     ${ques}
+     
       </div>
+  </div> 
+ <div  style="text-align: end">
+   <button onclick="show(1)" class="btn btn-primary">Next</button>
   </div>`
         })
         document.getElementById('quiz').innerHTML = str;
@@ -51,14 +57,58 @@ function renderQuiz(){
 }
 renderQuiz();
 
+function show(index){
+    question = questions[index];
+    let prevN = +index - 1;
+    let nextN = +index + 1;
+    let prevBtn = ''
+    if(index != 0){
+        prevBtn = `<button onclick="show(${prevN})" class="btn btn-primary">Prev</button>`
+    }
+    let nextBtn = ''
+    if(index < questions.length - 1){
+        nextBtn = `<button onclick="show(${nextN})" class="btn btn-primary">Next</button>`
+    }
+    let ques = ``;
+
+    let ans = '';
+    question.answers.forEach(answer => {
+        if(question.type === "radio"){
+            ans += ` <div class="form-check">
+              <input class="form-check-input" value="${answer.content}" type="radio" name="ques-${question.id}" id="answer-${answer.id}">
+              <label class="form-check-label" for="flexRadioDefault1">
+                  ${answer.title}.${answer.content}
+              </label>
+          </div>`
+        }
+        else {
+            ans += ` <div class="form-check">
+              <input class="form-check-input" value="${answer.content}" type="checkbox" name="ques-${question.id}" id="answer-${answer.id}">
+              <label class="form-check-label" for="flexRadioDefault1">
+                  ${answer.title}.${answer.content}
+              </label>
+          </div>`
+        }
+    })
+
+    ques += ` <h4 class="alert alert-primary disabled">${index+1}.${question.content}</h4>
+            ${ans}
+             <div  style="text-align: end">
+            ${prevBtn}
+            ${nextBtn}
+            </div>
+          `
+
+    document.getElementById("questions").innerHTML = ques
+}
+
 function submit(){
     let values = [];
     let isFull = true;
     for (let i = 1; i <= questions.length; i++) {
         let inputName = "ques-" + i;
-
         let inputElement = document.querySelectorAll('input[name="' + inputName + '"]:checked');
-
+        console.log(inputElement.length)
         if(inputElement.length<=0){
             isFull=false;
             break;
@@ -67,7 +117,7 @@ function submit(){
             let checkAns= [];
             if (inputElement[0].type === "radio") {
                 valueInput = inputElement[0].value;
-                answerO.answer = valueInput;
+                answerO.content = valueInput;
                 answerO.type = "radio"
                 values.push(answerO);
             } else  {
@@ -75,7 +125,7 @@ function submit(){
                 inputElement.forEach(function(checkbox) {
                     checkAns.push(checkbox.value);
                 });
-                answerO.answer = checkAns;
+                answerO.content = checkAns;
                 answerO.type = "checkbox"
                 values.push(answerO);
             }
@@ -88,33 +138,41 @@ function submit(){
     }
     console.log(values)
     console.log(questions)
-
 }
 
 function checkAnswer(value){
-    let count = 0;
-    let check = false;
     let score = 0;
-
     for (let i = 0; i < questions.length; i++) {
         if (value[i].type === "radio") {
             for (let j = 0; j < questions[i].answers.length; j++) {
-                if (questions[i].answers[j].title === value[i].answer && questions[i].answers[j].status === true)
+                if (questions[i].answers[j].content === value[i].content && questions[i].answers[j].status === true)
                     score++;
             }
         } else if (value[i].type === "checkbox") {
-            for (let j = 0; j < questions[i].answers.length; j++) {
-                for (let k = 0; k <questions[i].answers.length ; k++) {
-                    if (questions[i].answers[j].title === value[i].answer[k] && questions[i].answers[j].status === false){
-                        count++;
-                    }
-                }
+            let correctAns =[]
+
+                questions[i].answers.filter(ans => {
+                   if(ans.status === true){
+                       correctAns.push(ans.content)
+                   }
+            });
+            if(checkAnsCheckbox(correctAns,value[i].content)){
+                score++;
             }
-            if(count === 0){
-                score ++ ;
-            }
-            count = 0;
         }
     }
     return score;
+}
+function checkAnsCheckbox(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+    let ans = true;
+    for (let i = 0; i < array1.length; i++) {
+        if (array2.indexOf(array1[i]) === -1) {
+            ans = false;
+            break;
+        }
+    }
+    return ans;
 }
